73f86869215463c2fa75a72cbf7adf0f6a1701ed
Nick Margaritondo <86981780+Nickm615@users.noreply.github.com>
2022-01-25 09:14:08 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Slider from "react-slick";
+import fenway from "./assets/fenway.jpg";
+import minuteMaid from "./assets/minute-maid.jpg";
+import pnc from "./assets/pnc.jpg";
+import truist from "./assets/truist.jpg";
+import yankee from "./assets/yankee.jpg";
+
+const images = [fenway, minuteMaid, pnc, truist, yankee]
 
 function App() {
+  // react slick requires this settings object
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <prevArrow />
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider>
+        {images.map((img, index)=>(
+          <div>
+            <img src={img} alt={img} />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
